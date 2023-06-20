09fae57d65e3af61a3ece0ab67206eaa01b6d3f6
Yoojung Moon <yoojung131@gmail.com>
2023-06-20 23:50:53 +0900

    18. @NamedQuery, @Query 사용하기

--- demo/src/main/java/com/example/demo/TodoService.java
+++ demo/src/main/java/com/example/demo/TodoService.java
@@ -8,20 +8,22 @@ import com.example.demo.persistence.TodoRepository;
 
 @Service
 public class TodoService {
-
+	
 		@Autowired
 		private TodoRepository repository;
-
+		
 		public String testService(){
-		// Todo Entity 생성
-		TodoEntity entity = TodoEntity.builder().userId("user01").title("My first 
+			// Todo Entity 생성
+			TodoEntity entity = TodoEntity.builder().userId("user01").title("My first 
 todo item").build();
-		// Todo Entity 저장
-		repository.save(entity);
-		// Todo Entity 검색
-		TodoEntity savedEntity = 
-repository.findByUserId(entity.getUserId()).get(0);
-		return savedEntity.getUserId();
-	}
-
+			
+			//Todo Entity 저장
+			repository.save(entity);
+			
+			//Todo Entity 검색
+			TodoEntity savedEntity = 
+repository.searchByUserId(entity.getUserId()).get(0);
+			return savedEntity.getUserId();
+		
+		}
 }
